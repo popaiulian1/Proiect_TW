cb7486ca9c35ffcc760095e40f5f9380aee0aa12
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-10-27 13:11:51 +0200

    [added] Add CourseController with CRUD endpoints

---
+++ course/src/main/java/org/upstarters/course/controller/CourseController.java
@@ -0,0 +1,76 @@
+package org.upstarters.course.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.upstarters.course.dto.CourseDto;
+import org.upstarters.course.service.CourseService;
+
+@RestController
+@RequestMapping(path = "/courses")
+public class CourseController {
+
+    public final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping("/addCourse")
+    public ResponseEntity<CourseDto> addCourse(@RequestBody CourseDto courseDto) {
+        CourseDto addCourseDto = courseService.addCourse(courseDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(addCourseDto);
+    }
+
+    @GetMapping("/getAllCourses")
+    public ResponseEntity<Iterable<CourseDto>> getAllCourses() {
+        Iterable<CourseDto> courseDtos = courseService.getAllCourses();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(courseDtos);
+    }
+
+    @GetMapping("/getCourseById/{courseId}")
+    public ResponseEntity<CourseDto> getCourseById(@PathVariable Long courseId) {
+        return courseService.getCourseById(courseId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/updateCourse")
+    public ResponseEntity<String> updateCourse(@RequestBody CourseDto courseDto) {
+        Boolean isUpdated = courseService.updateCourse(courseDto);
+
+        if (isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body("Course updated successfully.");
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Course not found.");
+        }
+    }
+    
+    @DeleteMapping("/deleteCourse/{courseId}")
+    public ResponseEntity<String> deleteCourse(@PathVariable Long courseId) {
+        Boolean isDeleted = courseService.deleteCourse(courseId);
+
+        if (isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body("Course deleted successfully.");
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Course not found.");
+        }
+    }
+
+}

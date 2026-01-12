77ed51035e5f49cd44509bcae98b6675e4c940d2
Flaviu-Petre <flaviu.petre09@gmail.com>
2026-01-12 20:04:44 +0200

    Comment out @EnableMethodSecurity annotation
    The @EnableMethodSecurity annotation in SecurityConfig.java has been commented out, possibly to disable method-level security for the application.

--- course/src/main/java/org/upstarters/course/config/SecurityConfig.java
+++ course/src/main/java/org/upstarters/course/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+//@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
